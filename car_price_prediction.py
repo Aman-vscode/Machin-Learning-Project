292ae5c7bdce3be4e5d70c06507e50c448444c8d
Aman-vscode <proaman2005@gmail.com>
2026-06-23 13:01:00 +0530

    Update car_price_prediction.py

--- car_price_prediction.py
+++ car_price_prediction.py
@@ -11,7 +11,7 @@ def main():
         <h1>Car Price Prediction</h1>
     """
     DevName = """ 
-            <h3> This Project is Devloped by <u style= "text-decoration-color: red; text-decoration-thickness: 3px"> Aman Shukla </u> </h3>
+            <h3> This Project is Devloped by <u style= "text-decoration-color: blue; text-decoration-thickness: 3px"><p style= "color: red "> Aman Shukla </p></u> </h3>
     """
 
     model = xgb.XGBRegressor()
@@ -19,9 +19,9 @@ def main():
 
     st.markdown(html_temp, unsafe_allow_html=True)
 
-    st.markdown("This app will help you to predict your car selling price")
-
     st.markdown(DevName, unsafe_allow_html=True)
+    
+    st.markdown("This app will help you to predict your car selling price")
 
     p1 = st.number_input("Please enter ex-showroom price (In Lakhs)", 2.5,25.0,step=1.0)
 
